c0ea5a71cbe7b6a4a6293a876f300e8abd71ce0a
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2024-12-11 19:34:55 -0600

    started bootcamp and learning with self pace

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("TTTTT  K   K");
+        System.out.println("  T    K  K ");
+        System.out.println("  T    K K  ");
+        System.out.println("  T    KK   ");
+        System.out.println("  T    K K  ");
+        System.out.println("  T    K  K ");
+        System.out.println("  T    K   K");
+        
+    }
+}

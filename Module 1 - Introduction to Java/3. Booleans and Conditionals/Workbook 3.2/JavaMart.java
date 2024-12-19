ca1478f240db9b4f8222e64f35700b97e291a738
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2024-12-18 23:11:19 -0600

    learn operators

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -7,16 +7,30 @@ public class JavaMart {
  
         double toyCar = 5.99;
         System.out.println("Can I get this car?");
-       
+        if (wallet >= toyCar){
+            System.out.println("Sure!");
+            wallet -= toyCar;
+        }
         //if you have enough money
             //       print: Sure!
             //       pay for the toy car 
 
         //else:  Sorry, I only have <wallet> left.
 
+        else{
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
+        
         
         double nike = 95.99;
         System.out.println("Can I get these nike shoes?");
+        if (wallet >= nike){
+            System.out.println("Sure!");
+            wallet -= nike;
+        }
+        else{
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
         //if you have enough money
             //       print: Sure!
             //       pay for the nike shoes 

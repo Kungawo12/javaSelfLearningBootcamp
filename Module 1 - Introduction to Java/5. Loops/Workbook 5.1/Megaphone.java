dbe46867489564adb206675e48e11a7d416fe736
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2025-01-26 16:54:07 -0600

    new commit

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
@@ -1,6 +1,14 @@
 public class Megaphone {
     public static void main(String[] args) {
         // See Learn the Part for the instructions.
+
+        System.out.println("Prints message 10 times");
+        for (int i = 0; i < 10;i ++){
+            System.out.println("If Java was easy, they would call it Python");
+        }
+        System.out.println("\nNumber Each line");
+        for (int i = 1; i <= 10 ; i++){
+            System.out.println(i + ". If Java was easy, they would call it Python");
+        }
     }
-    
 }

b7fe0839666e3d169e1778cdf8edc805d57107d3
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2025-02-02 00:57:46 -0600

    gone through some practice with java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -1,8 +1,28 @@
+
+
 public class FizzBuzz {
     public static void main(String[] args) {
 
         // See Learn the Part for detailed instructions.
+        System.out.println("Learn The Part >>: java FizzBuzz");
 
+
+        for(int i= 0; i < 19; i++){
+            printingText(i);
+        }
+    }
+    public static void printingText(int i){
+        if ((i % 5 == 0) && ( i % 3 == 0 )){
+            System.out.println(i + " FizzBuzz");
+        }else if(i % 5 == 0){
+            System.out.println(i + " Buzz");
+        }else if(i % 3 ==0) {
+            System.out.println(i + " Fizz");
+        }else{
+            System.out.println(i);
+        }
+
+        
     }
 
 }

8f2fce6f6098819d6bf7323445624aa715045819
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2025-02-03 20:04:56 -0600

    learn java and done workbook challenge

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Beer {
     public static void main(String[] args) {
        // See detailed instructions on Learn the Part.
+        Scanner scan =  new Scanner(System.in);
+        System.out.print("Learn The Part >> ");
+        String song = scan.nextLine();
+        
+        for(int i = 99; i >=1; i --){
+            sing(i);
+        }
     }
 
+    public static void sing(int i) {
+        System.out.println(i +" bottles of beer on the wall," + i + " bottles of beer! ");
+        System.out.println("take one down, pass it around " + (i-1) + ", bottles of beer on the wall!");
+
+    }
 }
--- Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class EvenOrOdd {
     public static void main(String[] args) {
         // See Learn the Part for detailed instructions.
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Learn The Part >>: java ");
+        String Purpose = scan.nextLine();
+
+        for (int i =0; i < 20 ; i++){
+            evenOdd(i);
+        }
+    }
+    public static void evenOdd(int i ){
+        if(i % 2 ==0){
+            System.out.println(i + ": even");
+        }
+        else{
+            System.out.println(i + ": odd");
+        }
     }
 }
